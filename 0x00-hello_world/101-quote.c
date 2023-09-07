4cafdbf3b210bdf9d6b490c980d4891575a2ac58
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-07 12:38:28 -0700

    UNIX OS

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h> // Header for write function
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints a specific message to the standard error stream.
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    // This message will be printed
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    // we use the write function to output the message to stderr (file descriptor 2)
+    write(2, message, 59);
+
+    // Return 1 as required
+    return (1);
+}
