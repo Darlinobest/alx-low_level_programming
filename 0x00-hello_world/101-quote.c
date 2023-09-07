505714bef15b3f9c5f36911feef6c6a130d45815
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-07 12:46:02 -0700

    UNIX OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h> // Header for write function
+#include <unistd.h> /* Header for write function*/
 
 /**
  * main - Entry point of the program
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-    // This message will be printed
+    /* This message will be printed*/
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    // we use the write function to output the message to stderr (file descriptor 2)
+    /* we use the write function to output the message to stderr (file descriptor 2)*/
     write(2, message, 59);
 
-    // Return 1 as required
+    /* Return 1 as required*/
     return (1);
 }
