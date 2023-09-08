b2b9bccc36dd35af6b025a963227e7e7d03a7802
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-08 08:23:47 -0700

    A program that generates random numbers for both +ve and -ve number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ *  main - Entry point of the program
+ *
+ *  The functio generates random numbers for +ve, -ve and zero
+ *
+ *  Return: Always 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -12,19 +18,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	/* print positive number if n > 0 */
-	if(n > 0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf ("%d is positive\n",n);
 	}
 	/* print 0 if n == 0 */
-	else if(n == 0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf ("%d is zero\n",n);
 	}
 	/* print negative number if n < 0 */
 	else
 	{
-		printf("%d is negative\n",n);
+		printf ("%d is negative\n",n);
 	}
 	return (0);
 }
