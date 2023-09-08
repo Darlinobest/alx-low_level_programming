90a70b69de0cc42f5107dcc69350f6fa88600bda
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-08 08:07:28 -0700

    A proram that generates random numbers for both +ve and -ve

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	/* print positive number if n > 0 */
+	if(n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	/* print 0 if n == 0 */
+	else if(n == 0)
+	{
+		printf("%d is zero\n",n);
+	}
+	/* print negative number if n < 0 */
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+	return (0);
+}
