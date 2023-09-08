e24d460ae516371f643ef0cd76fa80f0d92443d6
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-08 09:42:30 -0700

    Generating random numbers and printing their last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,22 +20,22 @@ int main(void)
 	printf("Last digit of %d is ", n);
 
 	/* Calculating and extracting the last digit */
-	n = n%10;
+	n = n % 10;
 
 	/* condition for n greater than 5 */
-	if(n > 5)
+	if (n > 5)
 	{
 		printf("%d and is greater than 5\n", n);
 	}
 
 	/* condition for n equal to 0 */
-	if(n == 0)
+	if (n == 0)
 	{
 		printf("%d and is 0\n", n);
 	}
 
 	/* condition for n less than 6 and not equal to 0 */
-	if(n < 6 && n != 0)
+	if (n < 6 && n != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", n);
 	}
