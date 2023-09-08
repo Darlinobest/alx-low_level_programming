8c7ff6f2c8790f9f4e37155ee1e38014ffd16db0
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-08 10:04:40 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function displays a to z to the console
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* declare variable ch and initialized it to a */
+	char ch = 'a';
+
+	/* while loop condition to iterate through all the alpabets */
+	while (ch <= 'z')
+	{
+		/* function to print to the console output*/
+		putchar(ch);
+
+		/* to print a new line after each iteration */
+		putchar('\n');
+
+		/* to increment after printing letter a */
+		ch++;
+	}
+	return (0);
+}
