972ec3b05015a79fd6e75d5e0b21eb1ce1519b91
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-09 01:27:08 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	char ch = 'a';
-
-       	while (ch <= 'z')
+	
+	while (ch <= 'z')
        	{
-	       	if (ch == 'q' || ch == 'e')
+		if (ch == 'q' || ch == 'e')
 		{
 			ch++;
 			continue;
