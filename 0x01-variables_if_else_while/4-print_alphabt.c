120b88fc4bd242eee9b99664aa1c997ffa2882dc
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-10 01:56:59 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point for the program
+ * main - Entry point of the program
  *
- * This function prints letters to the console but skips letter q and e
+ * This program prints alphabets excepts q and e
  *
  * Return: Always 0
  */
 int main(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
-       	{
-		if (ch == 'q' || ch == 'e')
+	{
+		if (ch == 'e' || ch == 'q')
 		{
 			ch++;
 			continue;
