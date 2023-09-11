dbf5680d825ad8a3bf49c6acdcc08b7322049fa7
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-11 07:48:38 -0700

    A program that prints to std error output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" -" " Dora korpar, 2015-10-19\n";
-
-	write(2, message, 59);
-	return 1;
+	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	return (1);
 }
