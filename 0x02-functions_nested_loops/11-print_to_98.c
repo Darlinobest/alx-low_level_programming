58817369fcb8af017a96b0074a63d3343c672700
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-09-12 19:33:40 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				putchar(i + '0');
+				printf("%d, ", i);
 			else if (i == 98)
-				putchar(i + '0');
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				putchar(j + '0');
+				printf("%d, ", j);
 			else if (j == 98)
-				putchar(j + '0');
+				printf("%d\n", j);
 		}
 	}
 }
