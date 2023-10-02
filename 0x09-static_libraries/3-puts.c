35a07f9f57d102f9239e5d409a1747d5652aedcd
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-10-02 14:16:55 -0700

    created a the static library libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - print a string to standard output
+ * @str: pointer to character
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
