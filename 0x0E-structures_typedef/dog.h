86f127ec6d103df966cd51456cf7704a2d42b005
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-10-10 17:44:53 -0700

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - defines a new data type called dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
