066a8e1a913cff8de23a2274d1e1f98acbad7af1
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-10-11 18:14:47 -0700

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - function to create new dog
@@ -9,36 +8,44 @@
  * @owner: owner of the new dog
  * Return: pointer to the new dog, else NULL when it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int i, n_ame, o_wner;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
 	new_dog = malloc(sizeof(*new_dog));
-
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
-	new_dog->name = strdup(name);
-
-	if (new_dog->name == NULL)
+	if (new_dog == NULL || !(name) || !(owner))
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = strdup(owner);
-	if (new_dog->name == NULL)
+
+	for (n_ame = 0; name[n_ame]; n_ame++)
+		;
+
+	for (o_wner = 0; owner[o_wner]; o_wner++)
+		;
+
+	new_dog->name = malloc(n_ame + 1);
+	new_dog->owner = malloc(o_wner + 1);
+
+	if (!(new_dog->name) || !(new_dog->owner))
 	{
+		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+
+	for (i = 0; i < n_ame; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+
 	new_dog->age = age;
+
+	for (i = 0; i < o_wner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 
 	return (new_dog);
 }
