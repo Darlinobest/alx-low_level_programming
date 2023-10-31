8aed6ee7ee34bf8c9db7506a88b054487c59ba39
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-10-31 02:26:46 -0700

    a function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * str_concat - function to concatenate two strings
+ * @s1: first string to consider
+ * @s2: second string to be appended to s1
+ * Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len1, len2, result;
+	char *cat_str;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	result = len1 + len2;
+	if (len1 == 0 || len2 == 0)
+	{
+		s1 = " ";
+		s2 = " ";
+	}
+	cat_str = (char *)malloc(sizeof(int) * (result + 1));
+
+	if (cat_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		cat_str[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		cat_str[i + j] = s2[j];
+	}
+	cat_str[result] = '\0';
+
+	return (cat_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
