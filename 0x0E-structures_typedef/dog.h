3c88a715c6153a0dc8fabcc9ff518394d8de3c74
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-06 18:13:01 -0800

    a new type struct dog with informations

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - structure containing information of a dog
+ * @name: Dog name
+ * @owner: Dog owner
+ * @ae: age of the dog
+ */
+
 typedef struct dog
 {
 	char *name;
