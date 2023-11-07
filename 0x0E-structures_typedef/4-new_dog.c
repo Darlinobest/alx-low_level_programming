c0ad9abfd06a84dedb88c03195aaed7a4c61d255
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-06 20:57:24 -0800

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,46 @@
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
 	dog_t *new_dog;
+	int len_name, len_owner;
+
+	len_name = strlen(name);
+	len_owner = strlen(owner);
 
 	new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL || !(name) || !(owner))
+	new_dog->name = malloc(len_name + 1);
+        new_dog->owner = malloc(len_owner + 1);
+
+	if (!(new_dog->name) || !(new_dog->owner))
 	{
+		free(new_dog->name);
+		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = malloc(strlen(name) + 1);
-	new_dog->owner = malloc(strlen(owner) + 1);
-
-
-
-	if (new_dog->name == NULL)
-	{
-		free(new_dog->name);
-		return (NULL);
-	}
-	strcpy(new_dog->name, name);
-
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->owner);
-		return (NULL);
-	}
-	strcpy(new_dog->owner, owner);
+	for (i = 0; i < len_name; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	
 	new_dog->age = age;
+
+	for (i = 0; i < len_owner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 
 	return (new_dog);
 }
