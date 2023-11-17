ba69ca11df0f5cbec14796dacf84710ebc40fa91
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-16 23:33:09 -0800

    recent commit for task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
- * struct listss - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -14,13 +14,14 @@
  * ALX project
  */
 
-typedef struct listss
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct listss *next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
