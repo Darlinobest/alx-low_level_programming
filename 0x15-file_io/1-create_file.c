997150233a1f59b4f8d2f2fc5bc69a30e2cc1434
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-27 19:06:17 -0800

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and give it permissions
+ * for rw-------
+ * @filename: name of the file
+ * @text_content: NULL terminated string/content of the file
+ * Return: 1 upon success, -1 upon failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	ssize_t write_out;
+	ssize_t text_size;
+	mode_t permission;
+
+	permission = S_IRUSR | S_IWUSR;
+	text_size = strlen(text_content);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permission);
+
+	if (file_descriptor == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		write_out = write(file_descriptor, text_content, text_size);
+		if (write_out == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+	close(file_descriptor);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,13 @@
 #include <string.h>
 #include <stdio.h>
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 int _putchar(const char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
