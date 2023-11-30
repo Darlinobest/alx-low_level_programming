4a367e8b1bab94b2cd2e9ed5fd43cbfd93ab09ee
Darlington Kelechi Onwuemebolam <darlinobest@yahoo.com>
2023-11-30 03:54:16 -0800

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,23 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor;
 	ssize_t write_out;
 	ssize_t text_size;
-	mode_t permission;
 
-	permission = S_IRUSR | S_IWUSR;
-	if (text_content == NULL)
-		file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permission);
-	else
-	{
-		text_size = strlen(text_content);
-	}
+	if (!filename)
+		return (-1);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file_descriptor == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
+		text_size = strlen(text_content);
 		write_out = write(file_descriptor, text_content, text_size);
-		if (write_out == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+	}
+	if (write_out == -1)
+	{
+		close(file_descriptor);
+		return (-1);
 	}
 	close(file_descriptor);
 	return (1);
